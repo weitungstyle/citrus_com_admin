1ec575b1fcc50d3f23a17b2104e7c56784495d93
Victor Cheng <weitungstyle@gmail.com>
2023-02-11 14:33:22 +0800

    change headers authorization to token

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -30,7 +30,7 @@ export default {
   },
   created () {
     const loginToken = document.cookie.replace(/(?:(?:^|.*;\s*)loginToken\s*=\s*([^;]*).*$)|^.*$/, '$1')
-    this.$http.defaults.headers.common.Authorization = loginToken
+    this.$http.defaults.headers.common.Token = loginToken
   }
 }
 </script>
